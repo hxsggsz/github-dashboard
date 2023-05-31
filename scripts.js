2c52fdc3e81fbf90c2d4e76987c1efa978a91f28
hxsggsz <vt.hugo.2021@gmail.com>
2023-05-31 00:35:01 -0300

    build: search for an user with all data and validations

--- scripts.js
+++ scripts.js
@@ -1,22 +1,90 @@
 let defaultUser = 'hxsggsz'
 const form = document.getElementById('form')
 const input = document.getElementById('input')
-const error = document.getElementById('error')
+const name = document.getElementById('name')
+const img = document.getElementById('img')
+const date = document.getElementById('date')
+const userName = document.getElementById('username')
+const bio = document.getElementById('bio')
+const repos = document.getElementById('repos')
+const followers = document.getElementById('followers')
+const following = document.getElementById('following')
+const local = document.getElementById('location')
+const twitter = document.getElementById('twitter')
+const github = document.getElementById('github')
+const company = document.getElementById('company')
+
+function setUser(data) {
+  const formatedDate = new Intl.DateTimeFormat('en-US', { 
+      year: 'numeric',
+      day: 'numeric',
+      month: 'short',
+  })
+
+  const formatNumber = new Intl.NumberFormat('en-US', { notation: 'compact' })
+
+  name.textContent = data.name
+  img.src = data.avatar_url
+  img.alt = `avatar do ${data.login}`
+  bio.textContent = data.bio ? data.bio : 'this user does not have bio'
+  userName.textContent = `@${data.login}`
+  date.textContent = `Joined ${formatedDate.format(new Date(data.created_at))}`
+
+  repos.textContent = data.public_repos
+  followers.textContent = formatNumber.format(data.followers)
+  following.textContent = formatNumber.format(data.following)
+
+  local.innerHTML = data.location ? `
+    <i class="ph-bold ph-map-pin"></i>
+    <p>${data.location}</p>
+  ` : `
+    <i class="ph-bold ph-map-pin disabled"></i>
+    <p class="disabled">not available</p>
+  `
+  twitter.innerHTML = data.twitter_username ? `
+    <i class="ph-bold ph-twitter-logo"></i>
+    <p>${data.twitter_username}</p>
+    ` : `
+    <i class="ph-bold ph-twitter-logo disabled"></i>
+    <p class="disabled">not available</p>
+    `
+    github.innerHTML = `
+    <i class="ph-bold ph-link"></i>
+    <a href="${data.html_url}" target="_blank">
+    <p>${data.login}</p>	
+    </a>
+    `
+    company.innerHTML = data.company ? `
+    <i class="ph-bold ph-buildings"></i>
+    <p>${data.company}</p>
+    ` : `
+    <i class="ph-bold ph-buildings disabled"></i>
+    <p class="disabled">not available</p>  
+  `
+}
+
+async function firstLoad() {
+  try {
+    const res = await fetch('https://api.github.com/users/hxsggsz')
+    const data = await res.json()
+    setUser(data)
+  } catch (er) {
+    console.error(er)
+  }
+}
+firstLoad()
 
 async function getUser(user) {
   try {
     const res = await fetch(`https://api.github.com/users/${user === '' ? defaultUser : user}`)
-    return res.json()
+    const data = await res.json()
+    setUser(data)
   } catch (er) {
     console.error(er)
-    error.textContent = er.message
   }
 }
 
-getUser('')
-
 form.addEventListener('submit', async (ev) => {
   ev.preventDefault()
-  const user = await getUser(input.value)
-  console.log(user)
+  await getUser(input.value)
 })
